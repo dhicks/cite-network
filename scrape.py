177498e5d6364f63d809e505483a2a301a627e29
Dan Hicks <hicks.daniel.j@gmail.com>
2015-10-20 08:17:40 -0400

    work towards getting all articles from given sources

--- scrape.py
+++ scrape.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 import requests
 import json
+from math import ceil
 import xmltodict
 import time
 
-MY_API_KEY = '1f271dd2cf40387ab1d7e4645d36f599'#'6492f9c867ddf3e84baa10b5971e3e3d'#
+MY_API_KEY = '1f271dd2cf40387ab1d7e4645d36f599'
+#MY_API_KEY = '6492f9c867ddf3e84baa10b5971e3e3d'
 
 # Metadata to gather:  DOI, Scopus ID, author IDs, source ID, references
 
@@ -128,6 +130,7 @@ def get_meta_by_scopus(sid):
         return {'doi': doi, 'sid': sid, 'authors': authors, 'source': source, 'references': refs, 'raw': response_raw.text}
 
 def get_dois_by_issn(issn, after = '2004', before = '2016'):
+    # TODO: replace with the pubmed version from temp.py
     '''
     Retrieve DOIs for every article published in the source with the given ISSN
     :param issn: The source's ISSN
@@ -141,10 +144,10 @@ def get_dois_by_issn(issn, after = '2004', before = '2016'):
     base_query = 'http://api.elsevier.com/content/search/scopus?'
     # Search string:  
 	# (ISBN(issn) OR ISSN(issn)) AND PUBYEAR > after AND PUBYEAR < before			
-    search_string = (
+    search_string = ('query=' + 
         '(' + 
-            'query=ISBN(' + issn + ')' + '%20OR%20' + 
-            'query=ISSN(' + issn + ')' + 
+            'ISBN(' + issn + ')' + '%20OR%20' + 
+            'ISSN(' + issn + ')' #+ 
         ')' + '%20AND%20' + 
         'PUBYEAR%20%3E%20' + after + '%20AND%20' + 
         'PUBYEAR%20%3C%20' + before
@@ -152,15 +155,31 @@ def get_dois_by_issn(issn, after = '2004', before = '2016'):
     query = (base_query + search_string + '&' +
                 # We only need the DOI
                 'field=doi' + '&' + 
-                'count=1' + '&' +
-                'httpAccept=application%2Fjson' +
+                'httpAccept=application%2Fjson' + '&' +
                 'apiKey=' + MY_API_KEY)
-    # TODO: for some reason xmltodict doesn't like the xml that's being returned. 
-    # try getting json back instead
-    response_raw = requests.get(query, headers = {'X-ELS-APIKey': MY_API_KEY})
-    response = xmltodict.parse(response_raw.text)
-    total_papers = response['search-results']['opensearch:totalResults']
-    return(total_papers)
+    response_raw = requests.get(query + '&count=1')
+    response = json.loads(response_raw.text)
+    try:
+        total_papers = int(response['search-results']['opensearch:totalResults'])
+    except KeyError:
+        print(issn)
+        raise
+    total_pages = ceil(total_papers / max_per_page)
+    #return(total_pages)
+    dois = []
+    for page in range(total_pages):
+        start_at = max_per_page * page
+        this_query = (query + '&' + 
+                            'start=' + str(start_at) + '&' +
+                            'count=' + str(max_per_page))
+        #print(this_query)
+        response_raw = requests.get(this_query)
+        response = json.loads(response_raw.text)
+        for entry in response['search-results']['entry']:
+            dois += [entry['prism:doi']]
+    if len(dois) != total_papers:
+        raise ValueError("Number of DOIs retrieved doesn't match total_papers")
+    return(dois)
 
 
 start_time = time.time()
@@ -179,7 +198,7 @@ for doi in gen_0_doi:
     meta['core'] = True
     gen_0 += [meta]
     gen_n1_sid += meta['references']
-    if len(gen_0) % 100 == 0:
+    if len(gen_0) % 25 == 0:
         print(len(gen_0))
 gen_n1_sid = set(gen_n1_sid)
     
@@ -195,22 +214,24 @@ for sid in gen_n1_sid:
     meta['core'] = False
     gen_n1 += [meta]
     gen_n2_sid += meta['references']
-    if len(gen_n1) % 100 == 0:
+    if len(gen_n1) % 25 == 0:
         print(len(gen_n1))
 gen_n2_sid = set(gen_n2_sid)
 
 print(str(len(gen_n2_sid)) + ' items in generation -2')
 gen_n2 = []
-print('Retrieving metadata for generation -2')
-for sid in gen_n2_sid:
-    meta = get_meta_by_scopus(sid)
-    meta['core'] = False
-    gen_n2 += [meta]
-    if len(gen_n2) % 100 == 0:
-        print(len(gen_n2))
+#print('Retrieving metadata for generation -2')
+#for sid in gen_n2_sid:
+#    meta = get_meta_by_scopus(sid)
+#    meta['core'] = False
+#    gen_n2 += [meta]
+#    if len(gen_n2) % 100 == 0:
+#        print(len(gen_n2))
+
+core_ancestors = gen_0 + gen_n1 + gen_n2
 
 print()
-print('Totals from core set:')
+print('Totals for core set and ancestors:')
 print('Gen 0: ' + str(len(gen_0)))
 print('Gen -1: ' + str(len(gen_n1)))
 print('Gen -2: ' + str(len(gen_n2)))
@@ -218,18 +239,35 @@ print()
 
 
 # Step 3:  Get complete list of sources
-print('Collating sources')
+print('Collating sources for core set and ancestors:')
 sources = []
-for paper in gen_0 + gen_n1 + gen_n2:
-    sources += paper['source']
+for paper in core_ancestors:
+#for paper in papers:
+    this_source = paper['source']
+    if type(this_source) is str:
+        sources += [this_source]
+    if type(this_source) is list:
+        # Some papers have a list of variant ISBNs rather than a single number
+        sources += [this_source[0]]
 sources = set(sources)
 print(str(len(sources)) + ' distinct sources')
 
 
 # Step 4:  Get all articles in sources for 2005-2015
 
+print('Getting DOIs for each source')
+extended_set_dois = []
+for source in sources:
+    new_dois = get_dois_by_issn(source)
+    extended_set_dois += new_dois
+core_ancestor_dois = []
+for paper in core_ancestors:
+    core_ancestor_dois += [paper['doi']]
+new_dois = set(extended_set_dois) - set(core_ancestor_dois)
+
 
 # Step 5:  Write everything into a json file
+# TODO: for memory management reasons, save raw xml separately from everything else
 all_papers = gen_0 + gen_n1 + gen_n2
 outfile = 'papers.json'
 with open(outfile, 'w') as out:
